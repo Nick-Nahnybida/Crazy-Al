610a16f12e733f519f892d443d7c945a170f789a
Nick-Nahnybida <nicknahnybida@gmail.com>
2016-09-30 10:02:59 -0500

    Add files via upload

---
+++ Crazy Al/Crazy Al/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblCommissionRate = New System.Windows.Forms.Label()
+        Me.lblCommissionAmount = New System.Windows.Forms.Label()
+        Me.lblNetPay = New System.Windows.Forms.Label()
+        Me.txtSalesAmount = New System.Windows.Forms.TextBox()
+        Me.txtAdvancePayAmount = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Sales for the month:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(2, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Advance pay awarded:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Commission Rate:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(54, 135)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(65, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Commission:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(71, 165)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Net Pay:"
+        '
+        'lblCommissionRate
+        '
+        Me.lblCommissionRate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCommissionRate.Location = New System.Drawing.Point(125, 104)
+        Me.lblCommissionRate.Name = "lblCommissionRate"
+        Me.lblCommissionRate.Size = New System.Drawing.Size(100, 23)
+        Me.lblCommissionRate.TabIndex = 5
+        '
+        'lblCommissionAmount
+        '
+        Me.lblCommissionAmount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCommissionAmount.Location = New System.Drawing.Point(125, 135)
+        Me.lblCommissionAmount.Name = "lblCommissionAmount"
+        Me.lblCommissionAmount.Size = New System.Drawing.Size(100, 23)
+        Me.lblCommissionAmount.TabIndex = 6
+        '
+        'lblNetPay
+        '
+        Me.lblNetPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblNetPay.Location = New System.Drawing.Point(125, 165)
+        Me.lblNetPay.Name = "lblNetPay"
+        Me.lblNetPay.Size = New System.Drawing.Size(100, 23)
+        Me.lblNetPay.TabIndex = 7
+        '
+        'txtSalesAmount
+        '
+        Me.txtSalesAmount.Location = New System.Drawing.Point(125, 20)
+        Me.txtSalesAmount.Name = "txtSalesAmount"
+        Me.txtSalesAmount.Size = New System.Drawing.Size(100, 20)
+        Me.txtSalesAmount.TabIndex = 8
+        '
+        'txtAdvancePayAmount
+        '
+        Me.txtAdvancePayAmount.Location = New System.Drawing.Point(125, 49)
+        Me.txtAdvancePayAmount.Name = "txtAdvancePayAmount"
+        Me.txtAdvancePayAmount.Size = New System.Drawing.Size(100, 20)
+        Me.txtAdvancePayAmount.TabIndex = 9
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(14, 191)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 10
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(95, 191)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 11
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(176, 191)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblStatus.Location = New System.Drawing.Point(12, 217)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(239, 23)
+        Me.lblStatus.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(264, 240)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtAdvancePayAmount)
+        Me.Controls.Add(Me.txtSalesAmount)
+        Me.Controls.Add(Me.lblNetPay)
+        Me.Controls.Add(Me.lblCommissionAmount)
+        Me.Controls.Add(Me.lblCommissionRate)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Crazy Al's Commission Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents lblCommissionRate As System.Windows.Forms.Label
+    Friend WithEvents lblCommissionAmount As System.Windows.Forms.Label
+    Friend WithEvents lblNetPay As System.Windows.Forms.Label
+    Friend WithEvents txtSalesAmount As System.Windows.Forms.TextBox
+    Friend WithEvents txtAdvancePayAmount As System.Windows.Forms.TextBox
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+
+End Class
---
+++ Crazy Al/Crazy Al/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Variable declarations
+        Dim decSalesAmount As Decimal                   ' Monthly sales amount
+        Dim decAdvancePayAmount As Decimal              ' Advance pay taken
+        Dim decCommissionRate As Decimal                ' Commission rate
+        Dim decCommissionAmount As Decimal              ' Commission
+        Dim decNetPay As Decimal                        ' Net Pay
+
+        Try
+            ' Get the amount of sales.
+            decSalesAmount = CDec(txtSalesAmount.Text)
+
+            ' Get the amount of advance pay.
+            decAdvancePayAmount = CDec(txtAdvancePayAmount.Text)
+
+            ' Determine the commission rate.
+            Select Case decSalesAmount
+                Case Is < 10000
+                    decCommissionRate = 0.05D
+
+                Case 10000 To 14999
+                    decCommissionRate = 0.1D
+
+                Case 15000 To 17999
+                    decCommissionRate = 0.12D
+
+                Case 18000 To 21999
+                    decCommissionRate = 0.14D
+
+                Case Is >= 22000
+                    decCommissionRate = 0.15D
+            End Select
+
+            ' Calculate the commission and net pay amounts.
+            decCommissionAmount = decSalesAmount * decCommissionRate
+            decNetPay = decCommissionAmount - decAdvancePayAmount
+
+            ' Display the rate, commission, and net pay.
+            lblCommissionRate.Text = decCommissionRate.ToString("p")
+            lblCommissionAmount.Text = decCommissionAmount.ToString("c")
+            lblNetPay.Text = decNetPay.ToString("c")
+
+        Catch
+            ' Display an error message.
+            lblStatus.Text = "Please enter numeric values."
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtAdvancePayAmount.Clear()
+        txtSalesAmount.Clear()
+        lblCommissionAmount.Text = ("")
+        lblCommissionRate.Text = ("")
+        lblNetPay.Text = ("")
+        lblStatus.Text = ("")
+    End Sub
+End Class
